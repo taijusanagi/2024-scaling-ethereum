95cd62a8fd0d0fb4ccd72448022fe093be2fefc6
taijusanagi <taijusanagi@gmail.com>
2024-04-20 22:58:44 +0700

    add modal

--- app/src/pages/demo.tsx
+++ app/src/pages/demo.tsx
@@ -5,7 +5,7 @@ import { useMemo, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-import { useReadContract, useWriteContract, useAccount, usePublicClient } from "wagmi";
+import { useReadContract } from "wagmi";
 
 import deleGateWalletFactoryArtifact from "@/lib/artifacts/DeleGateWalletFactory.json";
 import deleGateWalletArtifact from "@/lib/artifacts/DeleGateWallet.json";
@@ -35,7 +35,21 @@ export default function Home() {
 
   const functionData = "0x61461954";
 
-  const publicClient = usePublicClient();
+  let defaultSteps = [
+    {
+      name: "Deploy Wallet",
+      description: "Deploy a new wallet contract with attestation-based role management",
+      status: "upcoming",
+    },
+    {
+      name: "Execute",
+      description: "Execute a function in a sample contract with attestation",
+      status: "upcoming",
+    },
+  ];
+
+  const [steps, setSteps] = useState(defaultSteps);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <main
@@ -128,6 +142,9 @@ export default function Home() {
               className="bg-purple-600 hover:bg-purple-800 disabled:hover:bg-purple-600 disabled:opacity-50 disabled:cursor-not-allowed text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
               onClick={async () => {
+                const result = [...defaultSteps];
+                setSteps(result);
+                setIsModalOpen(true);
                 const provider = new ethers.providers.JsonRpcProvider(rpc);
                 const code = await provider.getCode(deleGateWalletOwnerAddress);
                 if (code === "0x") {
@@ -136,19 +153,30 @@ export default function Home() {
                     deleGateWalletFactoryArtifact.abi,
                     signer
                   );
-                  const createDelegateWalletTx = await factory.createDelegateWallet(
-                    deleGateWalletOwnerAddress,
-                    signProtocolAddress,
-                    signProtocolSchemaId,
-                    ethers.constants.HashZero
-                  );
-                  console.log("createDelegateWalletTx.hash", createDelegateWalletTx.hash);
-                  await createDelegateWalletTx.wait();
+                  result[0].status = "current";
+                  setSteps([...result]);
+                  // const createDelegateWalletTx = await factory.createDelegateWallet(
+                  //   deleGateWalletOwnerAddress,
+                  //   signProtocolAddress,
+                  //   signProtocolSchemaId,
+                  //   ethers.constants.HashZero
+                  // );
+                  // console.log("createDelegateWalletTx.hash", createDelegateWalletTx.hash);
+                  // await createDelegateWalletTx.wait();
+
+                  // sleep 1
+                  await new Promise((resolve) => setTimeout(resolve, 1000));
+                  result[0].status = "complete";
+                  setSteps([...result]);
                 }
                 const wallet = new ethers.Contract(deleGateWalletOwnerAddress, deleGateWalletArtifact.abi, signer);
-                const executeTx = await wallet.execute(sampleAddress, "0x61461954", attestationId);
-                console.log("executeTx.hash", executeTx.hash);
-                await executeTx.wait();
+                result[1].status = "current";
+                setSteps([...result]);
+                // const executeTx = await wallet.execute(sampleAddress, "0x61461954", attestationId);
+                // console.log("executeTx.hash", executeTx.hash);
+                result[1].status = "complete";
+                setSteps([...result]);
+                // await executeTx.wait();
               }}
             >
               Test
@@ -156,6 +184,57 @@ export default function Home() {
           </div>
         </div>
       </div>
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center p-4">
+          <div className="bg-white p-6 rounded-md shadow-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto">
+            {" "}
+            <div className="flex flex-col">
+              {steps.map((step: any, index: number) => (
+                <div key={index} className="mb-4">
+                  <div className={`flex items-center text-sm ${index !== steps.length - 1 ? "mb-2" : ""}`}>
+                    <span
+                      className={`h-8 w-8 rounded-full flex items-center justify-center ${
+                        step.status === "complete"
+                          ? "bg-green-500"
+                          : step.status === "current"
+                          ? "bg-blue-500"
+                          : "bg-gray-200"
+                      }`}
+                    >
+                      {step.status === "complete" ? (
+                        <svg className="w-5 h-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+                        </svg>
+                      ) : (
+                        index + 1
+                      )}
+                    </span>
+                    <div className="ml-4">
+                      <p className={`font-semibold ${step.status === "current" ? "text-blue-600" : "text-gray-900"}`}>
+                        {step.name}
+                      </p>
+                      <p className={`text-xs ${step.status === "current" ? "text-blue-500" : "text-gray-500"}`}>
+                        {step.description}
+                      </p>
+                    </div>
+                  </div>
+                  {index !== steps.length - 1 && (
+                    <div className="ml-4 pl-1">
+                      <div className="h-full w-0.5 bg-gray-300"></div>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+            <button
+              onClick={() => setIsModalOpen(false)}
+              className="mt-4 w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-md font-bold text-white bg-purple-600 hover:bg-purple-700 focus:outline-none disabled:bg-purple-300 disabled:cursor-not-allowed disabled:opacity-50"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
