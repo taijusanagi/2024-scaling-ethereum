8250964c280a28894e7375536eadfbe1877c8d80
taijusanagi <taijusanagi@gmail.com>
2024-04-21 00:24:33 +0700

    submitted

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-verify";
 
 const accounts = [process.env.PRIVATE_KEY || ""];
 
@@ -15,6 +16,9 @@ const config: HardhatUserConfig = {
       url: "https://public.stackup.sh/api/v1/node/arbitrum-sepolia",
       accounts,
     },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   mocha: {
     timeout: 100000,
