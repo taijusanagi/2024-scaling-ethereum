16bbd5786efd97fd52d3f3466a7f40cdb858b7f5
taijusanagi <taijusanagi@gmail.com>
2024-04-20 23:45:20 +0700

    done

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -106,7 +106,7 @@ export default function Home() {
           <div className="flex flex-col ml-5 w-[63%] max-md:ml-0 max-md:w-full">
             <img
               loading="lazy"
-              src="/how-it-works.png"
+              src="/how-it-work.png"
               className="grow w-full shadow-xl max-md:mt-10 max-md:max-w-full"
             />
           </div>
